92cfe30f3f60086a0a55bbd9e1ec98264f41ceb7
MAG-BOSS <b18025@students.iitmandi.ac.in>
2020-12-16 17:09:52 +0530

    added like post functionality

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -11,9 +11,9 @@ const Posts = ({ setCurrentId }) => {
   
     return (
       !posts.length ? <CircularProgress /> : (
-        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-          {posts.map((post) => (
-            <Grid key={post._id} item xs={12} sm={6} md={6}>
+        <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+          {posts.map(( post ) => (
+            <Grid  key={post._id} item xs={12} sm={6} md={6}>
               <Post post={post} setCurrentId={setCurrentId} />
             </Grid>
           ))}
